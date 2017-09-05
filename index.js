53124615e1199e121c757b77629ec5320c71d45a
chrishansen69 <cadrekucra@gmx.com>
2017-09-04 21:41:23 -0300

    fix

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 'use strict';
 
-var pr;
-if (typeof Promise == 'undefined') {
-  var _g = (function*(...strs) {
-    var index = 0;
-    while (strs[index]) {
-      yield strs[index++];
-    }
-  })(
-    'any-promise',
-    'es6-promise',
-    'promise',
-    'native-promise-only',
-    'bluebird',
-    'rsvp',
-    'when',
-    'q',
-    'pinkie',
-    'lie',
-    'vow'
-  );
-  while (!pr) {
-    try {
-      pr = require(_g.next());
-    } catch (e) {/*empty*/}
-  }
-} else {
-  pr = Promise;
-}
+//var pr;
+//if (typeof Promise == 'undefined') {
+//  var _g = (function*(...strs) {
+//    var index = 0;
+//    while (strs[index]) {
+//      yield strs[index++];
+//    }
+//  })(
+//    'any-promise',
+//    'es6-promise',
+//    'promise',
+//    'native-promise-only',
+//    'bluebird',
+//    'rsvp',
+//    'when',
+//    'q',
+//    'pinkie',
+//    'lie',
+//    'vow'
+//  );
+//  while (!pr) {
+//    try {
+//      pr = require(_g.next());
+//    } catch (e) {/*empty*/}
+//  }
+//} else {
+//  pr = Promise;
+//}
 
-module.exports = (...args) => {
-  return pr.all(args);
+module.exports = (...promises) => {
+  return new Promise(async (resolve, reject) => {
+    let results = [];
+    for (let promise of promises.map(Promise.resolve, Promise)) {
+      results.push(await promise.then(async resolvedData => await resolvedData, reject));
+      if (results.length === promises.length) {
+        resolve(results);
+      }
+    }
+  });
 };
